04eb4e29927dadb8acffb21fe362380931fadc91
nathhje <n.van.sburg@hotmail.nl>
2018-10-30 16:35:26 +0100

    createSet

--- assignment1.py
+++ assignment1.py
@@ -6,6 +6,7 @@ Created on Tue Oct 30 15:44:09 2018
 """
 
 from random import random as rand
+import matplotlib.pyplot as plt
 
 def randPoint():
     
@@ -17,10 +18,50 @@ def randPoint():
 def checkMandelbrot(x,y):
     
     print("helloworld")
+    
+def createSet(s, i):
+    
+    fraction = 0
+    inSet = []
+    outSet = []
+    inSet.append([])
+    inSet.append([])
+    outSet.append([])
+    outSet.append([])
+    
+    for j in range(s):
+    
+        x,y = randPoint()
+        
+        check = checkMandelbrot(x,y,i)
+        
+        if check == True:
+            
+            fraction += 1
+            inSet[0].append(x)
+            inSet[1].append(y)
+        
+        else:
+            outSet[0].append(x)
+            outSet[1].append(y)
+            
+    return fraction, inSet, outSet
  
 def main():
     
-    x,y = randPoint()
+    
+    s = 1000
+    i = 1000
+    
+    fraction, xinSet, yinSet, outSet = createSet(s, i)
+    
+    print("The number of points in the set is %i/%i" %(fraction,s))
+    plt.figure()
+    plt.plot(inSet[0], inset[1], 'bo')
+    plt.plot(outSet[0], outSet[1], 'ro')
+    plt.show()
+    
+    
     
 if __name__ == "__main__":
     main()
