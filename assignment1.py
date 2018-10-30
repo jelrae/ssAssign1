41084e0153017d425e5a434b9aca433fc7b55db0
nathhje <n.van.sburg@hotmail.nl>
2018-10-30 16:16:11 +0100

    first functions

--- assignment1.py
+++ assignment1.py
@@ -5,3 +5,22 @@ Created on Tue Oct 30 15:44:09 2018
 @author: Jordan and Nathalie
 """
 
+from random import random as rand
+
+def randPoint():
+    
+    x = rand() * 3. - 2.
+    y = rand() * 3. - 1.5
+    
+    return x,y
+
+def checkMandelbrot(x,y):
+    
+    print("helloworld")
+ 
+def main():
+    
+    x,y = randPoint()
+    
+if __name__ == "__main__":
+    main()
